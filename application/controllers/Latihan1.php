741a0a6dec383554c3e3e4f7192632eebcf8b169
rismapudan <rismasilitonga002@gmail.com>
2024-03-31 16:15:41 +0700

    tugas 2

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,21 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<b><h2>Selamat Datang.. selamat belajar web programming</h2><b>";
+    }
+
+    public function penjumlahan($n1, $n2) 
+{ 
+    $this->load->model('Latihan2'); 
+    $data['nilai1'] = $n1; 
+    $data['nilai2'] = $n2; 
+    $data['hasil'] = $this->Latihan2->jumlah($n1, $n2); 
+
+    $this->load->view('Latihan3', $data); 
+    
+        
+    } 
+}
+
