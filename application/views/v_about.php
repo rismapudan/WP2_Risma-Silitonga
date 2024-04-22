0c9c5d6c4b34666a0a84eb1787de7bd099061de9
rismapudan <rismasilitonga002@gmail.com>
2024-04-22 15:58:26 +0700

    Pertemuan 4 tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Risma</li> 
+        <li>Nama Belakang : Silitonga</li> 
+</ul> 
+<br> 
+<h4>Alamat</h4> 
+<ul type="none"> 
+    <li> Sepakat 1 No. 25 </li> 
+</ul> 
+
+<h4>Tempat Lahir</h4> 
+<ul type="none"> 
+    <li>Sintang</li> 
+</ul> 
+
+<h4>Olah Raga Favorit</h4> 
+<ul type="square"> 
+    <li>Berenang</li> 
+    <li>Basket</li> 
+</ul> 
+</section>
