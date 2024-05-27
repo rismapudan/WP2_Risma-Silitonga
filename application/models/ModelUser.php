6cb9860f5695630ff45ec43e95aadaa832183c04
rismapudan <rismasilitonga002@gmail.com>
2024-05-27 15:54:08 +0700

    Pertemuan 10 dan Tugas 8

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') or exit('No direct script access allowed'); 
 
 class ModelUser extends CI_Model 
-{ 
+ { 
     public function simpanData($data = null) 
     { 
         $this->db->insert('user', $data); 
